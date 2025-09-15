01c310289d2ac0a94486a77c04c5d20f934883c1
harshsaini2375 <harshsaini2375@gmail.com>
2025-09-15 20:55:17 +0530

    update resume

--- app/page.js
+++ app/page.js
@@ -41,7 +41,7 @@ export default function Home() {
             <div className="istphoto w-full md:mt-0  mt-5 md:w-[30vw] h-[36vh] md:h-[45vh]  rounded-xl overflow-hidden"><Image width={900} height={900} className=" h-fit w-fit object-cover" src="/webdev.gif" alt="randompic" unoptimized /></div>
 
           </div>
-          <a href="/resume.pdf" download="Harsh saini Resume.pdf" className="w-fit" >
+          <a href="/resume.pdf" download="Harsh_saini_Resume.pdf" className="w-fit" >
             <button type="button" className="resume md:ml-[75px]  mt-5 text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Download My Resume</button> </a>
 
         </div>
